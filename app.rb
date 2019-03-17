9dc2825be8e53ee87ce2ecb1f545f244885e297c
Rachel Hawa <rachel.hawa@gmail.com>
2019-03-17 22:32:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,6 +11,13 @@ module FormsLab
     end
 
     post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |args|
+        Ship.new(args)
+      end
+      @ships = Ship.all
+
       erb :'pirates/show'
     end
   end
