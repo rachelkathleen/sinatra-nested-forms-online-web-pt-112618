5233e23eb2931edc94dc0d7e9bf3b65ccae980e3
Rachel Hawa <rachel.hawa@gmail.com>
2019-03-17 20:27:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      erb :root
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'pirates/new'
+    end
 
+    post '/pirates' do
+      erb :'pirates/show'
+    end
   end
 end
