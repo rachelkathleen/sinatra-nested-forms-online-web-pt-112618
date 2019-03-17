cb6e02adbce3261d4e7c1ec58a6cf25609ffdbe1
Rachel Hawa <rachel.hawa@gmail.com>
2019-03-17 22:38:24 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,15 +1,16 @@
 class Pirate
-  attr_accessor :name, :weight, :height
-  @@all = []
+  attr_reader :name, :weight, :height
 
-  def initialize(name, weight, height)
-    @name = name
-    @weight = weight
-    @height = height
-    @@all << self
+  PIRATES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATES << self
   end
 
   def self.all
-    @@all
+    PIRATES
   end
 end
